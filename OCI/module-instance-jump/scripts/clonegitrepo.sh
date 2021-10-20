a927affd5ee3ca5f7988df54dcf502cd8dd2f0f9
csbisht <csbisht1@gmail.com>
2021-10-20 16:37:50 +0530

    Update clonegitrepo.sh
    change owner

--- OCI/module-instance-jump/scripts/clonegitrepo.sh
+++ OCI/module-instance-jump/scripts/clonegitrepo.sh
@@ -3,3 +3,4 @@ cd /home/ubuntu/
 git clone https://github.com/csbisht/K8s-Lab-Questions.git
 sleep 3
 sudo chmod +x /home/ubuntu/K8s-Lab-Questions/*.sh
+sudo chown -R ubuntu:ubuntu /home/ubuntu/K8s-Lab-Questions
